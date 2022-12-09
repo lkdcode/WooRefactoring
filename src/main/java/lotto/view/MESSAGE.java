56fb1f3cb58fbeeaeb9a2d7b397e2281f43ef890
lkdcode <dlrlejr1@naver.com>
2022-12-09 19:13:10 +0900

    🔥feat : 메시지 출력 이넘 클래스
    추가 목록
    TICKETS ("개를 구매했습니다.")
    TOTLA_RATE ("총 수익률은 ")

--- src/main/java/lotto/view/MESSAGE.java
+++ src/main/java/lotto/view/MESSAGE.java
@@ -3,7 +3,7 @@ package lotto.view;
 //✨feat : 메시지 출력 이넘 클래스
 public enum MESSAGE {
     INPUT_MONEY("구입금액을 입력해주세요."),
-    LOTTERY("개를 구매했습니다."),
+    TICKETS("개를 구매했습니다."),
     INPUT_NUMBERS("당첨 번호를 입력해주세요."),
     INPUT_BONUS_NUMBERS("보너스 번호를 입력해주세요."),
     RATE("당첨 통계"),
@@ -13,11 +13,11 @@ public enum MESSAGE {
     FIVE_CONTAINS("5개 일치 (1,500,000원) - "),
     FIVE_BONUS_CONTAINS("5개 일치, 보너스 볼 일치 (30,000,000원) - "),
     SIX_CONTAINS("6개 일치 (2,000,000,000원) - "),
+    TOTAL_RATE("총 수익률은 "),
     ERROR_PAY("[ERROR] 숫자만 입력해주세요"),
     ERROR_RANGE("[ERROR] 1부터 45까지의 숫자만 입력해주세요."),
     ERROR_LENGTH("[ERROR] 6개의 숫자를 입력해주세요."),
     ERROR_DUPLICATION("[ERROR] 중복된 번호없이 숫자를 입력해주세요."),
-    ERROR_COMMA("[ERROR] 콤마(,)를 사용해서 번호를 구분해주세요."),
     ERROR_PAY_MIN("[ERROR] 최소 구매금은 1,000원 입니다."),
     ERROR_PAY_MAX("[ERROR] 최대 구매금은 10억원 입니다."),
     ERROR_PAY_UNIT("[ERROR] 구매금의 최소 단위는 1,000원 입니다."),
