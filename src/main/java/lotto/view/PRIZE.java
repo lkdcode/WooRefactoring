909137b6fa99092a6666cbf6113d2a761cf331cf
lkdcode <dlrlejr1@naver.com>
2022-12-09 19:14:36 +0900

    🔥feat : 등수와 당첨금 이넘 클래스

--- src/main/java/lotto/view/PRIZE.java
+++ src/main/java/lotto/view/PRIZE.java
@@ -1,19 +1,16 @@
 package lotto.view;
 
 //✨feat : 등수와 당첨금 이넘 클래스
-public enum PRIZE_MONEY {
-    SIX_CONTAINS(1, 2_000_000_000),
-    FIVE_BONUS_CONTAINS(2, 30_000_000),
-    FIVE_CONTAINS(3, 1_500_000),
-    FOUR_CONTAINS(4, 50_000),
-    THREE_CONTAINS(5, 5_000);
+public enum PRIZE {
+    FIRST(6, 2_000_000_000),
+    SECOND(5, 30_000_000),
+    THIRD(5, 1_500_000),
+    FOURTH(4, 50_000),
+    FIFTH(3, 5_000);
     private final int win;
     private final int prizeMoney;
 
-    // ##, ###
-    // 수익률 150_000.5%
-
-    PRIZE_MONEY(int win, int prizeMoney) {
+    PRIZE(int win, int prizeMoney) {
         this.win = win;
         this.prizeMoney = prizeMoney;
     }
