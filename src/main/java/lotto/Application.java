1bb0fba2168884f9a786576613fd223b43a28274
lkdcode <dlrlejr1@naver.com>
2022-12-09 19:11:13 +0900

    ✨feat : 재연동+

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -3,12 +3,11 @@ package lotto;
 import lotto.controller.SystemController;
 
 public class Application {
+
     public static void main(String[] args) {
 
         SystemController join = new SystemController();
         join.store();
-
-
 
     }
 }
