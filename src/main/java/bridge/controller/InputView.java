0ae7601c2d7854401030b8d7640baf2fdffaf01c
lkdcode <dlrlejr1@naver.com>
2022-12-11 01:50:39 +0900

    ✨feat : 사용자로부터 입력을 받는 클래스

---
+++ src/main/java/bridge/controller/InputView.java
@@ -0,0 +1,42 @@
+package bridge.controller;
+
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
+/**
+ * 사용자로부터 입력을 받는 역할을 한다.
+ */
+public class InputView {
+    private static final InputView INSTANCE = new InputView();
+    private static final ValidateInput VALIDATE_INPUT = new ValidateInput();
+
+    private InputView() {
+    }
+
+    /**
+     * 인스턴스 생성
+     */
+    public static InputView getInstance() {
+        return INSTANCE;
+    }
+
+    /**
+     * 다리의 길이를 입력받는다.
+     */
+    public int readBridgeSize() {
+        return VALIDATE_INPUT.validateInputBridgeSize(readLine());
+    }
+
+    /**
+     * 사용자가 이동할 칸을 입력받는다.
+     */
+    public String readMoving() {
+        return VALIDATE_INPUT.validateInputMove(readLine());
+    }
+
+    /**
+     * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
+     */
+    public String readGameCommand() {
+        return VALIDATE_INPUT.validateInputCommand(readLine());
+    }
+}
