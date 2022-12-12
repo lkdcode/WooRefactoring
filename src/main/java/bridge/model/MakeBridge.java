11407b59dc5a72eb2cd7798b0ebddd34e6d76eb3
lkdcode <dlrlejr1@naver.com>
2022-12-12 22:41:05 +0900

    🔥feat : 다리를 생성해주는 클래스
    게터 삭제

--- src/main/java/bridge/model/MakeBridge.java
+++ src/main/java/bridge/model/MakeBridge.java
@@ -3,16 +3,13 @@ package bridge.model;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Bridge {
+public class MakeBridge {
     private final BridgeRandomNumberGenerator BRIDGE_RANDOM_NUMBER_GENERATOR = new BridgeRandomNumberGenerator();
     private final BridgeMaker BRIDGE_MAKER = new BridgeMaker(BRIDGE_RANDOM_NUMBER_GENERATOR);
     private List<String> bridge = new ArrayList<>();
 
-    public void makeBridge(int size) {
+    public List<String> makeBridge(int size) {
         bridge = BRIDGE_MAKER.makeBridge(size);
-    }
-
-    public List<String> getBridge() {
         return bridge;
     }
 
