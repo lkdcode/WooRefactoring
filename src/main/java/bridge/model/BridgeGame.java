585272d964fd361a572b6964b1d5f18aa39a9bdb
lkdcode <dlrlejr1@naver.com>
2022-12-12 22:41:41 +0900

    🔥feat : 다리 게임을 관리하는 클래스
    move 값과 생성된 다리 값을 입력받아 비교하는 메소드 구현

--- src/main/java/bridge/model/BridgeGame.java
+++ src/main/java/bridge/model/BridgeGame.java
@@ -1,5 +1,6 @@
 package bridge.model;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -7,18 +8,49 @@ import java.util.List;
  */
 public class BridgeGame {
 
+    private final MakeBridge MAKE_BRIDGE = new MakeBridge();
+    private List<String> bridge = new ArrayList<>();
+    private String move;
+    private int trial = 0;
+    private int answer = 0;
+
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
      * <p>
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void move(String move) {
-
+    public boolean move(String move) {
+        this.move = move;
+        if (moveResult()) {
+            System.out.println("맞췄당");
+            System.out.println(answer + " 다음번 위치");
+            System.out.println(trial + " 시도 횟수");
+            return true;
+        }
+        return false;
     }
 
-    public void move(List<String> move) {
-
+    public void move(int size) {
+        this.bridge = MAKE_BRIDGE.makeBridge(size);
+        System.out.println(bridge+"정답");
     }
+
+    private boolean moveResult() {
+        if (moving()) {
+            answer++;
+            return true;
+        }
+        return false;
+    }
+
+    private boolean moving() {
+        trial++;
+        if (bridge.get(answer).equals(this.move)) {
+            return true;
+        }
+        return false;
+    }
+
 
     /**
      * 사용자가 게임을 다시 시도할 때 사용하는 메서드
@@ -28,4 +60,6 @@ public class BridgeGame {
     public void retry(String command) {
 
     }
+
+
 }
